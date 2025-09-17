b384ff961b09e5836871bdd6c11ec8dc67e9373a
Chen <daniel.chen4@hp.com>
2025-09-17 11:15:24 +0800

    Add benefit_plan column

--- backend/gemini_analyzer.py
+++ backend/gemini_analyzer.py
@@ -7,8 +7,9 @@ from pydantic import BaseModel
 
 
 class CreditCardReward(BaseModel):
-    # bank_name: str
-    # card_name: str
+    bank_name: str
+    card_name: str
+    benefit_plan: str
     store_name: str
     store_type_name: str
     reward_amount: float
@@ -40,7 +41,8 @@ class GeminiAnalyzer:
                 "response_schema": list[CreditCardReward],
             },
         )
-        return response.text
+        print(response.text)
+        return json.loads(response.text)
 
 def analyze_credit_card_text(input_text: str, api_key: str = None):
     analyzer = GeminiAnalyzer(api_key)
@@ -50,12 +52,12 @@ def analyze_credit_card_text(input_text: str, api_key: str = None):
 if __name__ == "__main__":
     load_dotenv()
     API_KEY = os.getenv('GEMINI_API_KEY')
-    with open("tmp\\raw", "r", encoding="utf-8") as f:
+    with open("tmp\\tmp", "r", encoding="utf-8") as f:
         text = f.read()
 
     try:
-        result = analyze_credit_card_text(text, api_key=API_KEY)
+        reward_dict = analyze_credit_card_text(text, api_key=API_KEY)
         with open("result\\cube.json", "w+", encoding="utf-8") as f:
-            json.dump(result, f, indent=4)
+            json.dump(reward_dict, f, indent=4, ensure_ascii=False)
     except Exception as e:
         print(f"Error: {e}")
